6fd7f85aed8fc470bb37967c689c2664adb51747
RafinalHaque <99057380+RafinalHaque@users.noreply.github.com>
2023-02-26 17:37:56 -0500

    frontend basic

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,34 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    
+    {
+        title: 'Messaging',
+        url: '#',
+        cName: 'nav-links'
+    },
+    
+    {
+        title: 'Services',
+        url: '#',
+        cName: 'nav-links'
+    },
+    
+    {
+        title: 'Phone call',
+        url: '#',
+        cName: 'nav-links'
+    },
+    
+    {
+        title: 'Email',
+        url: '#',
+        cName: 'nav-links-mobile'
+    },
+    
+
+
+]
