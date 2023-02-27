33433fbe3bf77747cfd8623ce65e083bf8eb44fa
Mehbub Rohit <mehburohit@gmail.com>
2023-02-26 22:23:07 -0500

    changed name of the site

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -38,7 +38,7 @@ class Navbar extends Component {
     render() {
         return (
             <nav className="NavbarItems">
-                <h1 className='navbar-logo'>React<i className='fab fa-react'></i></h1>
+                <h1 className='navbar-logo'>Support Hub</h1>
                 <div className='menu-icon' onClick={this.handleClick}>
                     <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
                 </div>
