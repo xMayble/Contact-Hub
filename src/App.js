3da31b3229056f4e8e5f21472fbca5c43b97517d
Mayble <mehburohit@gmail.com>
2023-03-30 01:32:53 -0400

    body changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,16 @@ function App() {
   return (
     <div className="App">
      <Navbar></Navbar>
-     
+      <h1 className='title_heading'>Welcome to Support Hub!</h1>
+      <p className='title_paragraph'>A convenient service for the customer! Ask your problems 
+        away with our specialized tools such as direct messaging with
+        our talented Admins. Phone calls if you aren't the texting
+        type. Pictures and videos for more clarity!
+      </p>
+      <form className='title_form'>
+        <button className='signup'>Sign Up</button>
+        <button className='login'>Login</button>
+      </form>
     </div>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,38 +1,11 @@
 import React, { Component } from 'react'
 import { MenuItems } from "./MenuItems"
 import './Navbar.css'
-import {Button} from "../Button"
-import { auth, signInWithGoogle, signOut } from '../../Firebase'
 
 class Navbar extends Component {
     state = {
         clicked: false,
         isLoggedIn: false,
-    }
-
-    componentDidMount() {
-        // Set up a listener for authentication state changes
-        auth.onAuthStateChanged((user) => {
-            if (user) {
-                // User is signed in
-                this.setState({ isLoggedIn: true });
-            } else {
-                // User is signed out
-                this.setState({ isLoggedIn: false });
-            }
-        });
-    }
-
-    handleClick = () => {
-        this.setState({clicked: ! this.state.clicked})
-    }
-
-    handleSignIn = () => {
-        signInWithGoogle();
-    }
-
-    handleSignOut = () => {
-        signOut();
     }
 
     render() {
@@ -54,12 +27,6 @@ class Navbar extends Component {
                         )
                     })}
                 </ul>
-                {this.state.isLoggedIn ? (
-                    <Button onClick={this.handleSignOut}>Sign out</Button>
-                ) : (
-                    <Button onClick={this.handleSignIn}>Sign in with Google</Button>
-                )}
-                {this.state.isLoggedIn && <h3 style={{ margin: '0 10px' }}>{localStorage.getItem("name")}</h3>}
             </nav>
         )
     }
